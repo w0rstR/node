90cfa838a08fba642579d2fe3132ebea169c07ac
w0rstR <sat2151@gmail.com>
2022-02-16 17:35:04 +0200

    fixed

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 const path = require('path')
 const fs = require('fs')
 
-const onlineUsers=[
-    {name: "Maks", age: 22, city: "Lviv" },
+const onlineUsers = [
+    {name: "Maks", age: 22, city: "Lviv"},
 ]
 
-const inPersonUsers =[
-    {name: "Oleg", age: 19, city: "Kiev" },
+const inPersonUsers = [
+    {name: "Oleg", age: 19, city: "Kiev"},
 ]
 
-function append(dirName, fileName, data){
-    const ap = data.map(item=>{
-        for(let key in item){
-            fs.appendFile(path.join(__dirname,'main',dirName,fileName),`${key.toUpperCase()}:${item[key]}\n`,(err)=>{
-                if(err){
-                    console.log(err)
+
+function append(dirMain, dirName, fileName, data) {
+    for (let item of data) {
+        for (let key in item) {
+            fs.appendFile(path.join(__dirname, dirMain, dirName, fileName), `${key.toUpperCase()}:${item[key]}\n`, (err) => {
+                if (err) {
+                    return console.log(err)
                 }
             })
         }
-    })
+    }
 }
-append('inPerson','inPerson.txt',onlineUsers)
-append('online','online.txt',inPersonUsers)
+
+
+fs.mkdir(path.join(__dirname, 'main'), (error) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    fs.mkdir(path.join(__dirname + '/main', 'inPerson'), (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+    })
+
+    fs.mkdir(path.join(__dirname + '/main', 'online'), (error) => {
+        if (error) {
+            return console.log(error)
+        }
+        append('main', 'inPerson', 'inPerson.txt', onlineUsers)
+        append('main', 'online', 'online.txt', inPersonUsers)
+    })
+
+})
