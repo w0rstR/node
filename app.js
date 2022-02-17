1f3520a0198bfcd47995c4dafbc4bbf3b998ed8f
w0rstR <sat2151@gmail.com>
2022-02-18 00:38:08 +0200

    fixed appp

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const path = require('path');
 const fs = require('fs');
 
 const onlineUsers = [
-    {name: "Maks", age: 22, city: "Lviv"},
+    {name: 'Marta', age: 24, city: 'Lviv'},
+    {name: 'Oleg', age: 32, city: 'Lviv'},
+    {name: 'Kira', age: 30, city: 'Kiev'},
 ];
 
 const inPersonUsers = [
-    {name: "Oleg", age: 19, city: "Kiev"},
+    {name: 'Andriy', age: 21, city: 'Lviv'},
+    {name: 'Dima', age: 42, city: 'Ternopil'},
+    {name: 'John', age: 22, city: 'Lviv'},
 ];
 
 
@@ -15,32 +19,70 @@ function append(dirMain, dirName, fileName, data) {
         for (let key in item) {
             fs.appendFile(path.join(__dirname, dirMain, dirName, fileName), `${key.toUpperCase()}:${item[key]}\n`, (err) => {
                 if (err) {
-                    return console.log(err)
+                    console.log(err);
+                    throw err;
                 }
-            })
+            });
         }
     }
 }
 
 
-fs.mkdir(path.join(__dirname, 'main'), (error) => {
-    if (error) {
-        return console.log(error)
-    }
+// fs.mkdir(path.join(__dirname, 'main'), (error) => {
+//     if (error) {
+//         console.log(error);
+//         throw error;
+//     }
+//
+//     fs.mkdir(path.join(__dirname , 'main', 'inPerson'), (error) => {
+//         if (error) {
+//             console.log(error);
+//             throw error;
+//         }
+//         append('main', 'inPerson', 'inPerson.txt', inPersonUsers);
+//
+//     });
+//
+//     fs.mkdir(path.join(__dirname , 'main', 'online'), (error) => {
+//         if (error) {
+//             console.log(error);
+//             throw error;
+//         }
+//         append('main', 'online', 'online.txt', onlineUsers)
+//     });
+//
+// });
 
-    fs.mkdir(path.join(__dirname + '/main', 'inPerson'), (error) => {
-        if (error) {
-            return console.log(error)
+function overwriteData(curentDir,newDir){
+    fs.readdir((path.join(__dirname,'main',curentDir)),(error,files)=>{
+        if(error){
+            console.log(error);
+            throw error;
         }
-
-    })
-
-    fs.mkdir(path.join(__dirname + '/main', 'online'), (error) => {
-        if (error) {
-            return console.log(error)
-        }
-        append('main', 'inPerson', 'inPerson.txt', onlineUsers)
-        append('main', 'online', 'online.txt', inPersonUsers)
-    })
-
-})
+        files.forEach(file=>{
+            fs.readFile((path.join(__dirname,'main',curentDir,file)),(error,dataFile)=>{
+                if(error){
+                    console.log(error);
+                    throw error;
+                }
+                fs.readdir((path.join(__dirname,'main',newDir)),(error,fls)=>{
+                    if(error){
+                        console.log(error);
+                        throw error;
+                    }
+                    fls.forEach(f=>{
+                        fs.writeFile((path.join(__dirname,'main',newDir,f)),dataFile,(err)=>{
+                            if(err){
+                                console.log(err);
+                                throw err
+                            }
+                            console.log('finish');
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+overwriteData('online','inPerson');
+overwriteData('inPerson','online');
